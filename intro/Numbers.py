4fde6b616bb601f5d064872c10e76e10a3b5fae4
Hussein Sarea <zzzsssx0@gmail.com>
2021-06-18 23:05:27 +0300

    Update Numbers.py
    commit

---
+++ intro/Numbers.py
@@ -0,0 +1,20 @@
+#Arithmetic operations
+
+#Addition:          3 + 2
+#Subtraction:       3 - 2
+#Multiplication:    3 * 2
+#Division:          3 / 2
+#Floor Division:    3 // 2
+#Exponent:          3 ** 2
+#Modulus:           3 % 2
+
+print(f"classic division {17 / 3}") # classic division returns a float
+
+print(f"floor division {17 // 3}") # floor division discards the fractional part
+
+print(17 % 3) # the % operator returns the remainder of the division
+
+#With Python, it is possible to use the ** operator to calculate powers
+
+print(5 ** 2) # 5 squared
+print (2 ** 7) # 2 to the power of 7
