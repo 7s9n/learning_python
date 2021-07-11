98fe8ed6b59fd3f94a1632bd0bc9e052159f6c1a
Hussein Sarea <zzzsssx0@gmail.com>
2021-07-11 20:53:50 +0300

    commit

--- intro/working_with_json.py
+++ intro/working_with_json.py
@@ -1,3 +1,42 @@
 import json
-print('Hello')
 
+json_string = '''
+{
+  "states": [
+    {
+      "name": "Alabama",
+      "abbreviation": "AL"
+
+    }
+  ]
+}
+'''
+
+people_string = '''
+{
+    "People": [
+        {
+            "name":"Hussein Sarea",
+            "phone":"737660509",
+            "emails":["zzzsssx0@gmail.com"],
+            "has_licence":true
+        },
+        {
+            "name":"Moataz Sarea",
+            "phone":"737660509",
+            "emails":["test123@gmail.com"],
+            "has_licence":false
+        },
+        {
+            "name":"Ekram Sarea",
+            "phone":"737660509",
+            "emails":null,
+            "has_licence":false
+        }
+    ]
+}
+'''
+
+data = json.loads(people_string)
+
+print(data)
