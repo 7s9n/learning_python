45ebfe0037ec0e3ca96c8c1453d46061f918c86a
Hussein Sarea <zzzsssx0@gmail.com>
2021-07-12 05:56:08 +0300

    commit

--- intro/dictionaries.py
+++ intro/dictionaries.py
@@ -11,7 +11,7 @@ value = mydict['lang']
 print(value) #c++
 
 # value = mydict['city'] #raise error key doesn't exist
-# print(value) 
+# print(value)
 
 value = mydict.get('city')
 print(value) #None
@@ -94,3 +94,53 @@ print(mydict)
 # print(mydict) #error
 
 del mydict , mydict1 , mydict2
+
+
+## Can build up a dict by starting with the the empty dict {}
+## and storing key/value pairs into the dict like this:
+## dict[key] = value-for-that-key
+dict = {}
+dict['a'] = 'alpha'
+dict['g'] = 'gamma'
+dict['o'] = 'omega'
+
+print (dict)  ## {'a': 'alpha', 'o': 'omega', 'g': 'gamma'}
+
+print (dict['a'])     ## Simple lookup, returns 'alpha'
+dict['a'] = 6       ## Put new key/value into dict
+'a' in dict         ## True
+## print dict['z']                  ## Throws KeyError
+if 'z' in dict:
+    print (dict['z'])     ## Avoid KeyError
+    print (dict.get('z'))  ## None (instead of KeyError)
+
+  ## By default, iterating over a dict iterates over its keys.
+  ## Note that the keys are in a random order.
+for key in dict:
+    print (key)
+## prints a g o
+
+## Exactly the same as above
+for key in dict.keys():
+    print (key)
+
+## Get the .keys() list:
+print (dict.keys())  ## ['a', 'o', 'g']
+
+## Likewise, there's a .values() list of values
+print (dict.values())  ## ['alpha', 'omega', 'gamma']
+
+## Common case -- loop over the keys in sorted order,
+## accessing each key/value
+for key in sorted(dict.keys()):
+    print (key, dict[key])
+
+## .items() is the dict expressed as (key, value) tuples
+print( dict.items())  ##  [('a', 'alpha'), ('o', 'omega'), ('g', 'gamma')]
+
+## This loop syntax accesses the whole dict by looping
+## over the .items() tuple list, accessing one (key, value)
+## pair on each iteration.
+for k, v in dict.items():
+  print (k, '>', v)
+## a > alpha    o > omega     g > gamma
