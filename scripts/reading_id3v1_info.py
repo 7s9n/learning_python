c09a780b55b0f89a60f47e97ef5fda9f058c0bf6
Hussein Sarea <zzzsssx0@gmail.com>
2021-07-16 20:05:54 +0300

    commit

--- scripts/reading_id3v1_info.py
+++ scripts/reading_id3v1_info.py
@@ -16,41 +16,54 @@ out as follows...
 from pathlib import Path
 import json
 
-
 genres_file = Path('list_of_id3v1_genres.json')
-def is_id3v1(header):
-    return header == b'TAG'
+
 def get_genre(index: int) ->str:
     with genres_file.open('r') as genres:
         data = json.load(genres)
     return data.get(str(index))
 
-def extract_info(file , encoding='utf-8'):
+def extract_info(data , encoding='utf-8'):
     info = {
-        'Tag' : file.read(3),
-        'Title' : file.read(30),
-        'Artist' : file.read(30),
-        'Album' : file.read(30),
-        'Year' : file.read(4),
-        'Comment' : file.read(30),
+        'Tag': None,
+        'Title': None,
+        'Artist': None,
+        'Album': None,
+        'Year': None,
+        'Comment': None,
+        'Track': None,
+        'Genre':None,
     }
-    if not is_id3v1(info['Tag']):
-        raise TypeError('This function supports only id3v1')
-    track = 0
-    if info['Comment'][28] == 0:
-        track = info['Comment'][29]
-        info['Comment'] = info['Comment'][:29]
-
-    for key , value in info.items():
-        info[key] = value.decode(encoding , 'backslashreplace')
-    info['Genre'] = get_genre(index=file.read(1)[0])
-    info['Track'] = track
+    empty_field = b'\x00' * 30
+    if data[:3] != b'TAG':
+        raise TypeError('This function reads only id3v1')
+    else:
+        info['Tag'] = data[:3].decode(encoding , 'backslashreplace')
+    if data[3:33] != empty_field:
+        info['Title'] = data[3:33].decode(encoding , 'backslashreplace')
+    if data[33:63] != empty_field:
+        info['Artist'] = data[33:63].decode(encoding , 'backslashreplace')
+    if data[63:93] != empty_field:
+        info['Album'] = data[63:93].decode(encoding , 'backslashreplace')
+    if data[93:97] != b'\x00'*4:
+        info['Year'] = data[93:97].decode(encoding , 'backslashreplace')
+    if data[97:127] != empty_field:
+        if data[97:127][28] == 0:
+            info['Track'] = data[97:127][29]
+            info['Comment'] = data[97:126].decode(
+            encoding , 'backslashreplace') if data[97:126] != empty_field[:29] else None
+        else:
+            info['Track'] = 0
+            info['Comment'] = data[97:127].decode(
+            encoding , 'backslashreplace') if data[97:127] != empty_field[:29] else None
+    info['Genre'] = get_genre(data[127:128][0])
     return info
 
-mp3_file = Path(__file__).parent.parent.parent / 'Desktop' / 'music' / 'Chilly.mp3'
-tag_size = 128
-with mp3_file.open('rb') as fr:
-    fr.seek(-tag_size , 2)
-    info = extract_info(fr)
-    for key , value in info.items():
-        print(key , value)
+if __name__ == '__main__':
+    mp3_file = Path(__file__).parent.parent.parent / 'Desktop' / 'music' / 'chilly.mp3'
+    with mp3_file.open('rb') as rf:
+        rf.seek(-128 , 2)
+        data = rf.read()
+        info = extract_info(data)
+        for key , value in info.items():
+            print(key , value)
